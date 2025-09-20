0cacf2e5784a48e1e387c60e922be70651ca25cd
adityagaur2620-netizen <aditygaur2620@gmail.com>
2025-09-21 01:15:42 +0530

    20-09

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let countries = [];
 let filteredCountries = [];
 const itemsPerPage = 6; 
 let currentPage = 1;
-let totalPages = 6;
+let totalPages = 8;
 async function fetchCountries() {
   try {
     const response = await fetch("https://api.allorigins.win/raw?url=https://www.apicountries.com/countries");
@@ -45,10 +45,10 @@ function showCountries() {
     container.appendChild(card);
   });
 }
+
 function setupPagination() {
   const pagination = document.getElementById("pagination");
   pagination.innerHTML = "";
-
 
   for (let i = 1; i <= totalPages; i++) {
     const btn = document.createElement("button");
@@ -62,4 +62,48 @@ function setupPagination() {
     pagination.appendChild(btn);
   }
 }
+function showDetails(country) {
+  const card= document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <div class="card-content">
+      <h2 style="color:white;">${country.name}</h2>
+      <img src="${country.flags.png}" style="width:150px;">
+      <p><b>Capital:</b>${country.capital}</p>
+      <p><b>Population:</b> ${country.population.toLocaleString()}</p>
+      <p><b>Region:</b> ${country.region}</p>
+      <p><b>Timezones:</b> ${country.timezones}</p>
+      <p><b>Currency:</b> ${country.currencies.map(c => c.name).join(", ")}</p>
+      <p><b>Borders:</b> ${country.borders}</p>
+      <p><b>Languages:</b> ${country.languages.map(l => l.name).join(", ")}</p>
+      <button id="closecard">Close</button>
+    </div>
+  `;
+
+  document.body.appendChild(card);
+  document.getElementById("closecard").onclick = () => card.remove();
+  card.addEventListener("click", e => {
+    if (e.target === card) card.remove();
+  });
+}
+
+document.getElementById("search").addEventListener("input", e => {
+  const q = e.target.value.toLowerCase();
+  filteredCountries = countries.filter(c =>
+    c.name.toLowerCase().includes(q)
+  );
+  currentPage =1;
+  showCountries();
+  setupPagination();
+});
+
+document.getElementById("refresh").addEventListener("click", () => {
+  document.getElementById("search").value = "";
+  filteredCountries = [...countries];
+  showCountries();
+  setupPagination();
+});
+
+fetchCountries()
 
